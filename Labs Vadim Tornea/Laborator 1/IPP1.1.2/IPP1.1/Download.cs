4c6115091773ca71570bfde343b33a700df92e4a
inkvirtual <inkvirtual@gmail.com>
2014-03-05 21:50:13 +0200

    added Vadim Tornea laboratory works

---
+++ Labs Vadim Tornea/Laborator 1/IPP1.1.2/IPP1.1/Download.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace IPP1._1
+{
+    class Download : IDownload
+    {
+        //function for downloading the images
+        public void GetImage(string fileName, string url)
+        {
+            string localFilename = @"D:\DownloadedImages\" + fileName + ".jpg";
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(url, localFilename);
+            }
+        }
+    }
+}
